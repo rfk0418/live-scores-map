fd3a38e78eb1770dfebe540d8bdfc3ba2d73000a
rfk0418 <kemnaryan@gmail.com>
2026-03-04 22:27:15 -0500

    Update teamlocations.js

--- teamlocations.js
+++ teamlocations.js
@@ -1,6 +1,8 @@
 const teamLocations = {
   "Atlanta Hawks": [33.7490, -84.3880],
   "Boston Celtics": [42.3601, -71.0589],
+  "Philadelphia 76ers": [39.9526, -75.1652 ],
+  "Memphis Grizzlies": [35.1486, -90.0519],
   "Brooklyn Nets": [40.6782, -73.9442],
   "Chicago Bulls": [41.8781, -87.6298],
   "Dallas Mavericks": [32.7767, -96.7970],
